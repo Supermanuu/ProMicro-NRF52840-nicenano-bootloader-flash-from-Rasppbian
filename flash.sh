443dcc27feafd5b90ffafbfeda1d85f39866888b
Supermanuu <manuel.pascuall@hotmail.com>
2024-09-22 22:02:22 +0100

    successfully flashed

---
+++ flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(dirname "$0")
+
+# stsrt openocd using this folder config file
+openocd -f "$SCRIPT_DIR/openocd.cfg" -c "flash write_image erase $SCRIPT_DIR/nice_nano_bootloader-0.6.0_s140_6.1.1.hex"
+
